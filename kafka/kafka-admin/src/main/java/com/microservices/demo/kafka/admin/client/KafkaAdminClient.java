464bc94aac3ca333d7f511bfbbfdf0609f1ca116
naivedeveloper95 <findsatyam95@gmail.com>
2023-06-05 00:41:55 +0530

    Added Changes for Kafka Consumers and Admin

---
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/client/KafkaAdminClient.java
@@ -0,0 +1,152 @@
+package com.microservices.demo.kafka.admin.client;
+
+import com.microservices.demo.config.KafkaConfigData;
+import com.microservices.demo.config.RetryConfigData;
+import com.microservices.demo.kafka.admin.exception.KafkaClientException;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+@Component
+public class KafkaAdminClient {
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaAdminClient.class);
+    private final KafkaConfigData kafkaConfigData;
+    private final RetryConfigData retryConfigData;
+    private final AdminClient adminClient;
+    private final RetryTemplate retryTemplate;
+    private final WebClient webClient;
+
+    public KafkaAdminClient(
+            WebClient webClient,
+            KafkaConfigData kafkaConfigData,
+            RetryConfigData retryConfigData,
+            AdminClient adminClient,
+            RetryTemplate retryTemplate
+    ) {
+        this.webClient = webClient;
+        this.kafkaConfigData = kafkaConfigData;
+        this.retryConfigData = retryConfigData;
+        this.adminClient = adminClient;
+        this.retryTemplate = retryTemplate;
+    }
+
+    public void createTopics() {
+        CreateTopicsResult createTopicsResult;
+        try {
+            createTopicsResult = retryTemplate.execute(this::doCreateTopics);
+        } catch (Throwable t) {
+            throw new KafkaClientException("Reached max number of retries for creating kafka topic(s)");
+        }
+        checkTopicsCreated();
+    }
+
+    private CreateTopicsResult doCreateTopics(RetryContext retryContext) {
+        List<String> topicNames = kafkaConfigData.getTopicNamesToCreate();
+        LOG.info("Creating {} topic(s), attempt {}", topicNames.size(), retryContext.getRetryCount());
+        List<NewTopic> kafkaTopics =  topicNames.stream().map(topic -> new NewTopic(
+                topic.trim(),
+                kafkaConfigData.getNumberOfPartitions(),
+                kafkaConfigData.getReplicationFactor()
+        )).collect(Collectors.toList());
+
+        return adminClient.createTopics(kafkaTopics);
+    }
+
+    private Collection<TopicListing> getTopics() {
+        Collection<TopicListing> topicListings;
+        try {
+            topicListings = retryTemplate.execute(this::doGetTopics);
+        } catch (Throwable t) {
+            throw new KafkaClientException("Reached max number of retries for reading kafka topic(s)", t);
+        }
+        return topicListings;
+    }
+
+    private Collection<TopicListing> doGetTopics(RetryContext retryContext) throws ExecutionException, InterruptedException {
+        LOG.info("Reading kafka topic(s) {}, attempt {}", kafkaConfigData.getTopicNamesToCreate().toArray(), retryContext.getRetryCount());
+        Collection<TopicListing> topicListings = adminClient.listTopics().listings().get();
+        if (topicListings != null) {
+            topicListings.forEach(topic -> LOG.debug("Topic with name {}", topic.name()));
+        }
+        return topicListings;
+    }
+
+    public void checkTopicsCreated() {
+        Collection<TopicListing> topicListings = getTopics();
+        int retryCount = 1;
+        Integer maxRetry = retryConfigData.getMaxAttempts();
+        Integer multiplier = retryConfigData.getMultiplier().intValue();
+        Long sleepTimeMs = retryConfigData.getSleepTimeMs();
+
+        for (String topic: kafkaConfigData.getTopicNamesToCreate()) {
+            while (!isTopicCreated(topicListings, topic)) {
+                checkMaxRetry(retryCount++, maxRetry);
+                sleep(sleepTimeMs);
+                sleepTimeMs *= multiplier;
+                topicListings = getTopics();
+            }
+        }
+    }
+
+    private boolean isTopicCreated(Collection<TopicListing> topicListings, String topicName) {
+        if (topicListings == null) {
+            return false;
+        }
+        return topicListings.stream().anyMatch(topic -> topic.name().equals(topicName));
+    }
+
+    private void sleep(Long sleepTimeMs) {
+        try {
+            Thread.sleep(sleepTimeMs);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Error while sleeping for waiting to create new topics!", e);
+        }
+    }
+
+    private void checkMaxRetry(int retry, Integer maxRetry) {
+        if (retry > maxRetry) {
+            throw new KafkaClientException("Reached max number of retries for reading kafka topic(s)");
+        }
+    }
+
+    public void checkSchemaRegistry() {
+        int retryCount = 1;
+        Integer maxRetry = retryConfigData.getMaxAttempts();
+        int multiplier = retryConfigData.getMultiplier().intValue();
+        Long sleepTimeMs = retryConfigData.getSleepTimeMs();
+
+        while (!getSchemaRegistryStatus().is2xxSuccessful()) {
+            checkMaxRetry(retryCount++, maxRetry);
+            sleep(sleepTimeMs);
+            sleepTimeMs *= multiplier;
+        }
+    }
+
+    private HttpStatus getSchemaRegistryStatus() {
+        try {
+            return webClient
+                    .method(HttpMethod.GET)
+                    .uri(kafkaConfigData.getSchemaRegistryUrl())
+                    .exchange()
+                    .map(ClientResponse::statusCode)
+                    .block();
+        } catch (Exception e) {
+            return HttpStatus.SERVICE_UNAVAILABLE;
+        }
+    }
+}
